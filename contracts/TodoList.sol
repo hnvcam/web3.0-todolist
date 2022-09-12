74447fc2f1c33b96909652ce7f23ab1e33f57a24
cam.nguyenhoangvi <hnvcam@gmail.com>
2022-09-12 16:25:12 +0700

    The todo list that whenever you do anything, please accept payment from your wallet. What's kind of Web3.0???

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,25 @@ pragma solidity ^0.8.9;
 
 contract TodoList {
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() {
+        createTask('Check out dappuniversity.com');
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
+    function toggleCompleted(uint taskId) public {
+        tasks[taskId].completed = !tasks[taskId].completed;
+    }
 }
